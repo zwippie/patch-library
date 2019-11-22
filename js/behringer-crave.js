47f43ba942ca41212639ce22f90f16f8f26bd1ba
Peter Gelderbloem <petersgelderbloem@gmail.com>
2019-11-22 21:56:13 +0000

    all knobs switches and connectors positioned

--- js/behringer-crave.js
+++ js/behringer-crave.js
@@ -8,41 +8,47 @@ const CANVAS_HEIGHT = 1035;
 const CABLE_WIDTH = 6;
 const CELL_RADIUS = 12;
 const CONNECTORS = [
-  {idx: 0, col: 0, row: 0, x: 1250, y: 88, name: "EXT. AUDIO", input: true},
-  {idx: 1, col: 0, row: 1, x: 1250, y: 154, name: "NOISE", input: false},
-  {idx: 2, col: 0, row: 2, x: 1250, y: 220, name: "VCO 1V/OCT", input: true},
-  {idx: 3, col: 0, row: 3, x: 1250, y: 285, name: "VCO MOD", input: true},
-  {idx: 4, col: 0, row: 4, x: 1249, y: 350, name: "MIX 1", input: true},
-  {idx: 5, col: 0, row: 5, x: 1249, y: 415, name: "MULT", input: true},
-  {idx: 6, col: 0, row: 6, x: 1249, y: 480, name: "GATE", input: true},
-  {idx: 7, col: 0, row: 7, x: 1249, y: 545, name: "TEMPO", input: true},
+    //inputs
+    //row 1
+        {idx: 0, col: 0, row: 0, x: 325, y: 110, name: "VCO 1V/OCT", input: true},
+        {idx: 1, col: 1, row: 0, x: 390, y: 110, name: "VCO LIN FM", input: true},
+        {idx: 2, col: 2, row: 0, x: 457, y: 110, name: "VCO MOD", input: true},
+        {idx: 3, col: 3, row: 0, x: 525, y: 110, name: "VCF CUTOFF", input: true},
+        {idx: 4, col: 4, row: 0, x: 590, y: 110, name: "VCF RES.", input: true},
+        {idx: 5, col: 5, row: 0, x: 655, y: 110, name: "MIX 1", input: true},
+        {idx: 6, col: 6, row: 0, x: 720, y: 110, name: "MIX 2", input: true},
+        {idx: 7, col: 7, row: 0, x: 785, y: 110, name: "VC MIX CTRL", input: true},
+        {idx: 8, col: 8, row: 0, x: 850, y: 110, name: "MULT", input: true},
+//row 2
+        {idx: 9, col: 0, row: 1, x: 325, y: 175, name: "MIX CV", input: true},
+        {idx: 10, col: 1, row: 1, x: 390, y: 175, name: "EXT. AUDIO", input: true},
+        {idx: 11, col: 2, row: 1, x: 457, y: 175, name: "TEMPO", input: true},
+        {idx: 12, col: 3, row: 1, x: 525, y: 175, name: "RUN/STOP", input: true},
+        {idx: 13, col: 4, row: 1, x: 590, y: 175, name: "RESET", input: true},
+        {idx: 14, col: 5, row: 1, x: 655, y: 175, name: "HOLD", input: true},
+        {idx: 15, col: 6, row: 1, x: 720, y: 175, name: "GATE", input: true},
+        {idx: 16, col: 7, row: 1, x: 785, y: 175, name: "VCA CV", input: true},
+        {idx: 17, col: 8, row: 1, x: 850, y: 175, name: "LFO RATE", input: true},
 
-  {idx: 8, col: 1, row: 0, x: 1315, y: 88, name: "MIX CV", input: true},
-  {idx: 9, col: 1, row: 1, x: 1315, y: 154, name: "VCF CUTOFF", input: true},
-  {idx: 10, col: 1, row: 2, x: 1315, y: 220, name: "VCO LIN FM", input: true},
-  {idx: 11, col: 1, row: 3, x: 1315, y: 285, name: "LFO RATE", input: true},
-  {idx: 12, col: 1, row: 4, x: 1313, y: 350, name: "MIX 2", input: true},
-  {idx: 13, col: 1, row: 5, x: 1313, y: 415, name: "MULT 1", input: false},
-  {idx: 14, col: 1, row: 6, x: 1313, y: 480, name: "EG", input: false},
-  {idx: 15, col: 1, row: 7, x: 1313, y: 545, name: "RUN/STOP", input: true},
+    //outputs
+    //row 1
+    {idx: 18, col: 9, row: 0, x: 915, y: 110, name: "MULT 1", input: false},
+    {idx: 19, col: 10, row: 0, x: 980, y: 110, name: "MULT 2", input: false},
+    {idx: 20, col: 11, row: 0, x: 1045, y: 110, name: "VCO PULSE", input: false},
+    {idx: 21, col: 12, row: 0, x: 1110, y: 110, name: "VCO SAW", input: false},
+    {idx: 22, col: 13, row: 0, x: 1177, y: 110, name: "EG", input: false},
+    {idx: 23, col: 14, row: 0, x: 1245, y: 110, name: "NOISE", input: false},
+    {idx: 24, col: 15, row: 0, x: 1310, y: 110, name: "VCA", input: false},
 
-  {idx: 16, col: 2, row: 0, x: 1380, y: 88, name: "VCA CV", input: true},
-  {idx: 17, col: 2, row: 1, x: 1380, y: 154, name: "VCF RES.", input: true},
-  {idx: 18, col: 2, row: 2, x: 1380, y: 220, name: "VCO SAW", input: false},
-  {idx: 19, col: 2, row: 3, x: 1380, y: 285, name: "LFO TRI", input: false},
-  {idx: 20, col: 2, row: 4, x: 1377, y: 350, name: "VC MIX CTRL", input: true},
-  {idx: 21, col: 2, row: 5, x: 1377, y: 415, name: "MULT 2", input: false},
-  {idx: 22, col: 2, row: 6, x: 1377, y: 480, name: "KB", input: false},
-  {idx: 23, col: 2, row: 7, x: 1377, y: 545, name: "RESET", input: true},
-
-  {idx: 24, col: 3, row: 0, x: 1445, y: 88, name: "VCA", input: false},
-  {idx: 25, col: 3, row: 1, x: 1445, y: 154, name: "VCF", input: false},
-  {idx: 26, col: 3, row: 2, x: 1445, y: 220, name: "VCO PULSE", input: false},
-  {idx: 27, col: 3, row: 3, x: 1445, y: 285, name: "LFO SQ", input: false},
-  {idx: 28, col: 3, row: 4, x: 1443, y: 350, name: "VC MIX", input: false},
-  {idx: 29, col: 3, row: 5, x: 1443, y: 415, name: "ASSIGN", input: false},
-  {idx: 30, col: 3, row: 6, x: 1443, y: 480, name: "GATE", input: false},
-  {idx: 31, col: 3, row: 7, x: 1443, y: 545, name: "HOLD", input: true}
+    //outputs
+    //row 2
+    {idx: 25, col: 9, row: 1, x: 915, y: 175, name: "LFO TRI", input: false},
+    {idx: 26, col: 10, row: 1, x: 980, y: 175, name: "LFO SQ", input: false},
+    {idx: 27, col: 11, row: 1, x: 1045, y: 175, name: "VC MIX", input: false},
+    {idx: 28, col: 12, row: 1, x: 1110, y: 175, name: "ASSIGN", input: false},
+    {idx: 29, col: 13, row: 1, x: 1177, y: 175, name: "KB", input: false},
+    {idx: 30, col: 14, row: 1, x: 1247, y: 175, name: "GATE", input: false},
+    {idx: 31, col: 15, row: 1, x: 1310, y: 175, name: "VCF", input: false},
 ]
 const KNOBS = [
   {idx: 0, x: 140, y: 293, type: "large", name: "FREQUENCY"},
@@ -62,19 +68,18 @@ const KNOBS = [
   {idx: 14, x: 140, y: 645, type: "large", name: "TEMPO/GATE LENGTH"}
 ]
 const KNOB_TYPES = {
-  large: {radius: 30, limit: 360}
+  large: {radius: 30, limit: 310}
 }
 const TOGGLES = [
-  {idx: 0, x: 268, y: 79, type: "twoway", name: "VCO WAVE"},
-  {idx: 1, x: 266, y: 218, type: "twoway", name: "VCO MOD SOURCE"},
-  {idx: 2, x: 541, y: 218, type: "twoway", name: "VCO MOD DEST"},
-  {idx: 3, x: 541, y: 352, type: "twoway", name: "LFO WAVE"},
-  {idx: 4, x: 681, y: 218, type: "twoway", name: "VCF MODE"},
-  {idx: 5, x: 821, y: 218, type: "twoway", name: "VCF MOD SOURCE"},
-  {idx: 6, x: 821, y: 352, type: "twoway", name: "SUSTAIN"},
-  {idx: 7, x: 986, y: 79, type: "twoway", name: "VCA MODE"},
-  {idx: 8, x: 1098, y: 218, type: "twoway", name: "VCF MOD POLARITY"},
-
+  {idx: 0, x: 300, y: 375, type: "twoway", name: "VCO WAVE"},
+  {idx: 1, x: 473, y: 375, type: "twoway", name: "VCO MOD SOURCE"},
+  {idx: 2, x: 645, y: 375, type: "twoway", name: "VCO MOD DEST"},
+  {idx: 3, x: 998, y: 475, type: "twoway", name: "LFO WAVE"},
+  {idx: 4, x: 825, y: 375, type: "twoway", name: "VCF MODE"},
+  {idx: 5, x: 998, y: 375, type: "twoway", name: "VCF MOD SOURCE"},
+  {idx: 6, x: 645, y: 475, type: "twoway", name: "SUSTAIN"},
+  {idx: 7, x: 1344, y: 375, type: "twoway", name: "VCA MODE"},
+  {idx: 8, x: 1980, y: 375, type: "twoway", name: "VCF MOD POLARITY"}
 ]
 const TOGGLE_TYPES = {
   twoway: {values: ["left", "right"]}
